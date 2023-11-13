1cc05e7ae0892c0a767c343e2fed02cebd3de488
Lucas Varela <hi@lucasvarela.dev>
2023-11-13 09:56:49 -0300

    fix: run npm.cmd instead of npm on windows

--- src/utils/install.ts
+++ src/utils/install.ts
@@ -2,10 +2,14 @@ import * as childProcess from 'node:child_process'
 
 export const installDependencies = (cwd: string): Promise<void> =>
   new Promise((resolve, reject) => {
-    const npm = childProcess.spawn('npm', ['install'], {
-      cwd,
-      stdio: 'inherit',
-    })
+    const npm = childProcess.spawn(
+      process.platform === 'win32' ? 'npm.cmd' : 'npm',
+      ['install'],
+      {
+        cwd,
+        stdio: 'inherit',
+      },
+    )
 
     npm.on('close', (code) => {
       if (code === 0) resolve()
